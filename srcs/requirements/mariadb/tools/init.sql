a485c947b12afb49a84fe29a85fa7a7475c5641f
chajax <chajax@chajax.42.fr>
2022-10-31 12:16:37 -0400

    first commit

--- srcs/requirements/mariadb/tools/init.sql
+++
@@ -1,9 +0,0 @@
--- Create ${DB_NAME}
-CREATE DATABASE IF NOT EXISTS wordpress;
--- Create new user wordpress
-CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED by '${WP_PASSWORD}';
--- Give access to all database wordpress to the user 'wordpress'
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' WITH GRANT OPTION;
--- Set up root password
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${PASSWORD}');
-FLUSH PRIVILEGES;
