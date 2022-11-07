8ae47f493d7cb2b6194490c18662a2ac73260708
chajax <chajax@chajax.42.fr>
2022-11-07 15:29:31 -0500

    first commit

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
 
-
-cat <<-EOF > /etc/mysql/my.cnf
-    [mysqld]
-    user = root
-    port = 3306
-    datadir = /var/lib/mysql
-    bind-address = 0.0.0.0
-    skip-bind-address
-    skip-networking = false
-    pid-file = /run/mysqld/mysqld.pid
-    socket = /run/mysqld/mysqld.sock
-EOF
-
 if  [ ! -d "/var/lib/mysql/$DB_NAME" ]
 then
     service mysql start
     /usr/bin/mysql_install_db
-	echo "Creating \"wordpress\" database with secure_install...\n"
+	echo "Creating \"wordpress\" database with mysql_install_db...\n"
     apt-get update
     apt-get install -y expect
     SECURE_MYSQL=$(expect -c "
@@ -36,15 +23,15 @@ then
     expect \"Reload privilege tables now?\"
     send \"y\r\"
     expect eof
-    ")
-    echo "$SECURE_MYSQL"
-    apt-get purge -y expect
-    echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$ADMIN_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-    echo "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO 'chajax'@'%' IDENTIFIED BY '$ADMIN_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-    echo "\"$DB_NAME\" database created.\n"
-    echo "Now importing wordpress_db dump to bypass installation process..."
-    mysql -uroot -p$ADMIN_PASSWORD $DB_NAME < /var/wordpress_db.sql
-    echo "Database dump import done!"
+        ")
+        echo "$SECURE_MYSQL"
+        apt-get purge -y expect
+        echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$ADMIN_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+        echo "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO 'chajax'@'%' IDENTIFIED BY '$ADMIN_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+        echo "\"$DB_NAME\" database created.\n"
+    #echo "Now importing wordpress_db dump to bypass installation process..."
+    #mysql -uroot -p$ADMIN_PASSWORD $DB_NAME < /var/wordpress_db.sql
+    #echo "Database dump import done!"
 else
     echo "Database already created! Silly you... :$\n"
 fi
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -4,9 +4,9 @@ FILE=/var/www/html/wp-config.php
 echo "Trying to create wp-config.php file..."
 if  [ ! -f "$FILE" ]; then
 	echo "Creating config..."
-	wp config create --dbname=$DB_NAME --dbuser=chajax --dbpass=$ADMIN_PASSWORD --dbhost="mariadb" --path="/var/www/html/" --allow-root --skip-check
-	wp core install --url="chajax.42.fr" --title="chajax awesome wordpress site of ses morts" --admin_user="chajax" --admin_password=$ADMIN_PASSWORD --admin_email="chajax@student.42.fr" --path="/var/www/html/" --allow-root
-	wp user create chajax chajax@student.42.fr --role=author --user_pass=$WP_PASSWORD --allow-root
+	wp config create --dbname=$DB_NAME --dbuser=$ADMIN_USER --dbpass=$ADMIN_PASSWORD --dbhost="mariadb" --path="/var/www/html/" --allow-root --skip-check
+	wp core install --url="chajax.42.fr" --title="I love Inception" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path="/var/www/html/" --allow-root
+	wp user create $USER $EMAIL_USER --role=author --user_pass=$WP_PASSWORD --allow-root
     echo "wp-config.php file created!!"
 else
     echo "wp-config.php already created."
